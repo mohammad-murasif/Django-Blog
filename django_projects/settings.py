99c9458523a4a2ea272545f4bd39543acc7055b1
Mursif <murasif99@gmail.com>
2023-01-08 16:48:47 +0530

    Final

--- django_projects/settings.py
+++ django_projects/settings.py
@@ -129,6 +129,12 @@ MEDIA_URL='/media/'
 CRISPY_TEMPLATE_PACK='bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL='login'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER='web.projects.22092001@gmail.com'
+EMAIL_HOST_PASSWORD='bwgrtlnolqpaiifd'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
